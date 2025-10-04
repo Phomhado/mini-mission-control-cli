e7819407cfc8bcf35ad790e11d3b8bd714d241e9
Pedro Oliveira <pedro.he.oli10@gmail.com>
2025-10-03 21:11:42 -0300

    feat: created new Structs and Enums for the mission report

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+enum Role {
+    Pilot,
+    Engineer,
+    Medic,
+    Scientist,
+    Commander,
+}
+
+enum DutyStatus {
+    OnDuty,
+    OffDuty,
+    EVA,
+    Resting,
+}
+
+struct CrewMember {
+    id: i64,
+    name: String,
+    role: Role,
+    duty_status: DutyStatus,
+}
+
+enum SystemName {
+    LifeSupport,
+    Propulsion,
+    Comms,
+    Power,
+    Navigation
+}
+
+enum SystemStatus {
+    Nominal,
+    Warning,
+    Critical,
+    Offline
+}
+
+struct ShipSystem {
+    id: i64,
+    name: SystemName,
+    status: SystemStatus,
+    power_draw_kw: f64,
+    last_check: String,
+}
+
+enum EventTag {
+    Burn,
+    Checklist,
+    Telemetry,
+    Bug,
+    CriticalBug,
+    Routine,
+    Improvement
+}
+struct MissionEvent {
+    t_plus_seconds: f64,
+    title: String,
+    description: String,
+    tags: Vec<EventTag>,
+}
+
+struct MissionClock {
+    time_start: String,
+    time_end: String,
+}
+
+fn main() {
+    let crew_member = CrewMember {
+        id: 1,
+        name: "Pedro Oliveira".to_string(),
+        role: Role::Engineer,
+        duty_status: DutyStatus::OnDuty,
+    };
+
+    let ship_system = ShipSystem {
+        id: 12,
+        name: SystemName::LifeSupport,
+        status: SystemStatus::Warning,
+        power_draw_kw: 1.0,
+        last_check: "10/03/2025".to_string(),
+    };
+
+    let mission_event = MissionEvent {
+        t_plus_seconds: 15.2,
+        title: "Fix bug on Life Support systems".to_string(),
+        description: "There is a bug that is causing the Life Support systems to turn off for around 2 seconds, we need to fix it".to_string(),
+        tags: vec![EventTag::CriticalBug, EventTag::Improvement],
+    };
+
+    let mission_clock = MissionClock {
+        time_start: "10/03/2025".to_string(),
+        time_end: "Still Doing".to_string(),
+    };
+}
